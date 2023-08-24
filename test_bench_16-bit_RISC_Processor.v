ccb77c4283b6891cbe580f383175d5ee8c6d002c
shivamverma060 <143109637+shivamverma060@users.noreply.github.com>
2023-08-24 21:00:49 +0530

    Add files via upload

---
+++ test_bench_16-bit_RISC_Processor.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+`include "Parameter.v"
+
+// FPGA projects, VHDL projects, Verilog projects 
+// Verilog code for RISC Processor 
+// Verilog testbench code to test the processor
+module test_Risc_16_bit;
+
+ // Inputs
+ reg clk;
+
+ // Instantiate the Unit Under Test (UUT)
+ Risc_16_bit uut (
+  .clk(clk)
+ );
+
+ initial 
+  begin
+   clk <=0;
+   `simulation_time;
+   $finish;
+  end
+
+ always 
+  begin
+   #5 clk = ~clk;
+  end
+
+endmodule
+
+`ifndef PARAMETER_H_
+`define PARAMETER_H_
+
+// FPGA projects, VHDL projects, Verilog projects 
+// Verilog code for RISC Processor 
+// Parameter file
+`define col 16 // 16 bits instruction memory, data memory
+`define row_i 15 // instruction memory, instructions number, this number can be changed. Adding more instructions to verify your design is a good idea.
+`define row_d 8 // The number of data in data memory. We only use 8 data. Do not change this number. You can change the value of each data inside test.data file. Total number is fixed at 8. 
+`define filename "./test/50001111_50001212.o"
+`define simulation_time #160
+
+`endif
